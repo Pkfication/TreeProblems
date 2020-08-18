402927a55485a4ced5b5da493895f47a1da20888
Pkfication <pankajgupta221b@gmail.com>
2020-08-18 23:21:23 +0530

    Create InsertInBST.rb

---
+++ InsertInBST.rb
@@ -0,0 +1,43 @@
+class Node
+  attr_accessor :val, :left, :right
+  def initialize(val = 0, left = nil, right = nil)
+    @val = val
+    @left = left
+    @right = right
+  end
+end
+
+def insert(root, node)
+  if root.nil?
+    root = node
+  end
+  if root.val > node.val
+    if root.left.nil?
+      root.left = node
+    elsif
+      insert(root.left, node)
+    end
+  else
+    if root.right.nil?
+      root.right = node
+    else
+      insert(root.right, node)
+    end
+  end
+end
+
+def inorder(root)
+  if root
+    inorder(root.left)
+    print root.val, ' '
+    inorder(root.right)
+  end
+end
+
+r = Node.new(50)
+insert(r, Node.new(20))
+insert(r, Node.new(40))
+insert(r, Node.new(70))
+insert(r, Node.new(60))
+insert(r, Node.new(80))
+inorder(r)
